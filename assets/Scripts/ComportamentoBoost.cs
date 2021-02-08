0e41d36b4fba462bae35f3ea02ede61f2b16e199
Grégori Sabel <seriosabel57@gmail.com>
2021-02-07 21:09:26 -0300

    tudo

---
+++ assets/Scripts/ComportamentoBoost.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComportamentoBoost : MonoBehaviour
+{
+
+	// impacto do alvo no jogo
+	public int pontuacao = 0;
+	public float tempoExtra = 5.0f;
+	public float tempoMetralhadora = 3;
+
+	// explode quando atingido?
+	public GameObject prefabExplosao;
+
+	// quando colidir com outro gameObject...
+	void OnCollisionEnter (Collision newCollision)
+	{
+		// não faça nada se houver um game manager e o jogo já acabou
+		if (GameManager.gm) {
+			if (GameManager.gm.gameIsOver)
+				return;
+		}
+		// Se foi atingido por um projétil...
+		if (newCollision.gameObject.tag == "Player") {
+			if (prefabExplosao) {
+				// Instancia a explosão na posição e rotação do alvo
+				Instantiate (prefabExplosao, transform.position, transform.rotation);
+			}
+			
+			// se o game manager existir, altere o tempo e placar conforme o alvo
+			if (GameManager.gm) {
+				GameManager.gm.targetHit (pontuacao, tempoExtra, tempoMetralhadora);
+			}
+				
+			
+				
+			// autodestrua-se
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ assets/Scripts/Movimento.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float velocidade = 0.5f;
+    public bool emX;
+    public bool emY;
+    public bool emZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emX = (Random.value > 0.6f);
+        emY = (Random.value > 0.6f);
+        emZ = (Random.value > 0.6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (emX)
+        {
+            gameObject.transform.Translate(Vector3.right * velocidade * Mathf.Cos(Time.timeSinceLevelLoad), Space.World);
+        }
+        if (emY)
+        {
+            gameObject.transform.Translate(Vector3.back * velocidade * Mathf.Cos(Time.timeSinceLevelLoad), Space.World);
+        }
+        if (emZ)
+        {
+            gameObject.transform.Translate(Vector3.up * velocidade * Mathf.Cos(Time.timeSinceLevelLoad), Space.World);
+        }
+     
+    }
+}
